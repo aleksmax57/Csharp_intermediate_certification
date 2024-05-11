049e51576dd767c2bdc25e75c150b7686af306d4
Aleksander <aleksan-makar@yandex.ru>
2024-05-11 18:35:14 +0700

    Commit Program.cs in ExamFils

---
+++ ExamFils/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение M и N. Напишите программу, которая выводит вввсе натуральные
+// числа в промежутке от M до N. Использовать рекурсию, не использовать цикл
+
+Console.Write("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());  
+Console.Write("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void PrintNumbers(int N)
+{
+    if(N < M) return;
+    PrintNumbers(N - 1);
+    Console.Write(N + " ");
+}
+void Maim()
+{
+    PrintNumbers(N);
+}
+Maim();
